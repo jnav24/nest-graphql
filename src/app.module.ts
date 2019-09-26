e5a1bf19a301d9a22c26e37cd4af48ab33d7d121
Justin Navarro <info@justinnavarro.net>
2019-09-26 18:16:36 -0400

    install mongoose and setup mongoose in project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MongooseModule} from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRoot('mongodb://localhost:27017/heroes'),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
